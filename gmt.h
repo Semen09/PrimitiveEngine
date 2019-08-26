f328a5b6ea9cdf2c051ad32d070a4f9111c9914f
Semen09 <999solom.s.v.999@gmail.com>
2019-08-27 00:24:12 +0300

    Added draw triangle behaviour

--- gmt.h
+++ gmt.h
@@ -13,6 +13,12 @@ template <class t> struct Vec2 {
 	};
 	Vec2() : u(0), v(0) {}
 	Vec2(t _u, t _v) : u(_u), v(_v) {}
+
+	void Swap(Vec2& p) {
+		Vec2 temp = *this;
+		*this = p;
+		p = temp;
+	}
 	inline Vec2<t> operator +(const Vec2<t>& V) const { return Vec2<t>(u + V.u, v + V.v); }
 	inline Vec2<t> operator -(const Vec2<t>& V) const { return Vec2<t>(u - V.u, v - V.v); }
 	inline Vec2<t> operator *(float f)          const { return Vec2<t>(u * f, v * f); }
--- main.cpp
+++ main.cpp
@@ -3,17 +3,26 @@
 #include "model.h"
 #include <vector>
 #include <iostream>
+#include <array>
+#include <algorithm>
 #include <math.h>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 Model* model = NULL;
-const int width = 2000;
-const int height = 2000;
+const int width = 100;
+const int height = 100;
 
 struct Point {
 	int x;
 	int y;
+
+	Point() : x(0), y(0)
+	{};
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
 };
 
 template <typename T>
@@ -56,10 +65,67 @@ void DrawLine(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& c
 	}
 }
 
+class Triangle {
+public:
+	Triangle(const Vec2i& first, const Vec2i& second, const Vec2i& third) {
+		points[0] = first;
+		points[1] = second;
+		points[2] = third;
+
+		if (points[0].y > points[1].y) points[0].Swap(points[1]);
+		if (points[0].y > points[2].y) points[0].Swap(points[2]);
+		if (points[1].y > points[2].y) points[1].Swap(points[2]);
+	}
+
+	void Draw(TGAImage& image, const TGAColor& color) {
+		DrawLine(points[0].x, points[0].y, points[1].x, points[1].y, image, color);
+		DrawLine(points[2].x, points[2].y, points[1].x, points[1].y, image, color);
+		DrawLine(points[2].x, points[2].y, points[0].x, points[0].y, image, white);
+	}
+
+	bool InTriangle(Vec2i P)
+	{
+		P = P - points[0];
+		Vec2i B = points[1] - points[0];
+		Vec2i C = points[2] - points[0];
+
+		// Zero division if
+		if (C.y == 0) {
+			C.Swap(B);
+		}
+
+		float w1 = (float)(P.y * C.x - P.x * C.y) / (B.y * C.x - B.x * C.y);
+		float w2 = ((float)P.y - w1 * B.y) / C.y;
+
+		return w1 >= 0 && w2 >= 0 && (w1 + w2) <= 1;
+	}
+
+	void BetterDraw(TGAImage& image, const TGAColor& color) {
+		int xMax, xMin;
+		if (points[0].x > points[1].x) {
+			xMax = (points[0].x > points[2].x) ? points[0].x : points[2].x;
+			xMin = (points[1].x > points[2].x) ? points[2].x : points[1].x;
+		}
+		else {
+			xMax = (points[1].x > points[2].x) ? points[1].x : points[2].x;
+			xMin = (points[0].x > points[2].x) ? points[2].x : points[0].x;
+		}
+		for (int x = xMin; x <= xMax; x++) {
+			for (int y = points[0].y; y <= points[2].y; y++) {
+				if (InTriangle({ x, y })) {
+					image.set(x, y, red);
+				}
+			}
+		}
+	}
+private:
+	std::array<Vec2i, 3> points;
+};
+
 int main(int argc, char** argv) {
 	model = new Model("obj/african_head/african_head.obj");
 	TGAImage image(width, height, TGAImage::RGB);
-	for (int i = 0; i < model->nfaces(); i++) {
+	/*for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
 		for (int j = 0; j < 3; j++) {
 			Vec3f v0 = model->vert(face[j]);
@@ -70,7 +136,14 @@ int main(int argc, char** argv) {
 			int y1 = (v1.y + 1.) * height / 2.;
 			DrawLine(x0, y0, x1, y1, image, white);
 		}
-	}
+	}*/
+	Triangle a({ 0, 0 }, { 30, 30 }, { 50, 20 });
+	Triangle b({90, 90}, {50, 80}, {70, 50});
+
+	a.BetterDraw(image, red);
+	b.BetterDraw(image, red);
+	//a.Draw(image, red);
+	//b.Draw(image, red);
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
